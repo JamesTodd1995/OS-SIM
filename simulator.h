f561bff0defa178c07c35179c6b6186c732965ff
TurtleDynasty <dm667@nau.edu>
2017-04-09 16:28:14 -0700

    Code standard improvements to simulator.h

--- simulator.h
+++ simulator.h
@@ -1,3 +1,11 @@
+// Simulator Program Header Information////
+/**
+  * @file simulator.h
+  *
+  * @brief header for simulator.c that defines PCB_H, loads dependencies, prototypes functions
+  */
+
+
 #ifndef PCB_H
 #define PCB_H
 
@@ -5,7 +13,7 @@
 #include "parMethods.h"
 #include <string.h>
 
-
+// data types
 struct pcb
   {
      struct Node *first;
@@ -30,54 +38,66 @@ struct forThread
      struct forThread *waitPrt;
    };
 
+struct logNode *addLogNode(struct logNode *current);
 
 enum massage_codes { unk = -1000, printTM, printTL, printB, qtTimedOut,
                      commandEnded, setToBlocked, isLock, isUnlock, allBlocked,
                      notAllBlocked, NULL_PROCESS_ID, waitQueueSignal, allExit,
                      memoryTimeOut};
 
-
-
-
-
+// function prototypes
 int startSim(struct Node *metaData,struct configStruct configData);
+
 int getStart(struct Node *data, int cTimeMultipler);
+
 void setProcessesNew(struct pcb *process,struct Node *data, struct configStruct configData);
+
 void setProcessesReady(struct pcb *process, int numOfProcess);
+
 int  getNumberOfProcess(struct Node *metaData);
 
 int readProcess
 (
-struct Node **first,struct configStruct configData, double *time, int count,int printType,
-int numberArraySize, char *printingLine, char *numberArray, struct logNode **currentLN,
-int *pcbTime, struct forThread *arrayPrt, int numOfProcess, struct pcb *pcbArray
+     struct Node **first, struct configStruct configData, double *time, int count, int printType,
+     int numberArraySize, char *printingLine, char *numberArray, struct logNode **currentLN,
+     int *pcbTime, struct forThread *arrayPrt, int numOfProcess, struct pcb *pcbArray
 );
 
 int startAction
 (
-char comL,char *opStr,int *cyT,int proT,int iOT,double *time,int count,int printType,
-int numberArraySize, char *printingLine, char *numberArray, struct logNode **currentLN,
-struct configStruct configData, int *pcbTime, struct forThread *arrayPrt,
-int numOfProcess
+     char comL,char *opStr,int *cyT,int proT,int iOT,double *time,int count,int printType,
+     int numberArraySize, char *printingLine, char *numberArray, struct logNode **currentLN,
+     struct configStruct configData, int *pcbTime, struct forThread *arrayPrt,
+     int numOfProcess
 );
 
 
 int pickProcess
 (
-struct pcb array[], struct configStruct configData, int size,
-struct forThread *data
+     struct pcb array[], struct configStruct configData, int size,
+     struct forThread *data
 );
-struct logNode *addLogNode(struct logNode *current);
+
 void writeToLog (struct logNode *first,struct configStruct configData);
 
 int getNextIndexInWaitQueue(struct forThread *data, int size);
+
 int getFCFSN(struct pcb array[], int size);
+
 int getSJFN(struct pcb array[], int size);
+
 int getFCFSP(struct pcb array[], int size);
+
+int getRRP(struct pcb array[], int size);
+
 void threadHandler(struct forThread *data);
+
 void setToNull(struct forThread *data, int size);
+
 int countWaitQueue(struct forThread *data, int size);
+
 void unblockProcess(struct pcb array[], int index, int subTime);
+
 void setIndexToNull (struct forThread *data, int index);
 
 #endif
