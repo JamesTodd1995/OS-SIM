4bfff76fe2e14762f73695bebc23f35f2fbd1fc7
TurtleDynasty <dm667@nau.edu>
2017-04-09 15:22:27 -0700

    Coding standards improvements to mainprog.h

--- mainprog.h
+++ mainprog.h
@@ -1,3 +1,10 @@
+// Main Program Header ////
+/**
+  * @file mainprog.h
+  *
+  * @brief header for mainprog.c that defines MAINPRO_H, loads dependencies, prototypes functions
+  */
+
 #ifndef MAINPRO_H
 #define MAINPRO_H
  
@@ -5,12 +12,8 @@
 #include "myTimer.h"
 #include "simulator.h"
 
-
-
 void doesFileExist(char* fileName);
 void printConfigData(struct configStruct *configData);
-
-
 
 #endif
 
